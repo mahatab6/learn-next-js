b4bd4c7769c433494551dbfb94b4e69f62cdb6ac
mahatab6 <mahatabcall56@gmail.com>
2025-07-27 23:34:47 +0600

    logical router making

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,33 @@
+"use client"
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 export default function AboutPage() {
+
+    // active user logical implemant
+
+    const router = useRouter();
+    const isLoggedIn = false;
+    const handleNavigation = () => {
+        if(isLoggedIn){
+            router.push("/about/address");
+        } else{
+            router.push("/")
+        }
+    }
+
+
+
   return (
     <div>
         <p>AboutPage</p>
+
+        <p>
+            <Link href='/about/address'>address</Link>
+        </p>
+
+        <button className=' cursor-pointer' type='button' onClick={handleNavigation} >Address</button>
     </div>
   )
 }
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,19 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
     return (
         <nav className='w-10/12 mx-auto'>
           <ul className='flex justify-between'>
+            <Link href='/'>
             <li>Home</li>
+            </Link>
+            <Link href='/services'>
             <li>Services</li>
+            </Link>
+            <Link href='/about'>
             <li>About</li>
+            </Link>
           </ul>
         </nav>
     );
